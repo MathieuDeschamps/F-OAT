323dfd81a49e99672270d864edb722c6270e7f16
LeclechM <32573436+LeclechM@users.noreply.github.com>
2018-02-21 14:38:40 +0100

    remove controls from mediaElement

--- client/views/components/videoPlayer/videoPlayer.js
+++ client/views/components/videoPlayer/videoPlayer.js
@@ -1,23 +1,23 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import {Projects} from '../../../../lib/collections/Project.js';
+import  '/public/renderers/vimeo.js';
 import './videoPlayer.html';
-import '/public/renderers/vimeo.js';
-
-import {videoControler} from '../videoControler/videoControler.js';
+import './videoPlayer.css';
 
 
 var Player;
 Template.videoPlayer.onRendered(function () {
    $('video').mediaelementplayer({
       pluginPath:'/packages/johndyer_mediaelement/',
+      features: '[]',
+      clickToPlayPause : false,
       success: function (mediaElement, domObject) {
         Player =mediaElement;
         mediaElement.setSrc((Projects.findOne(Router.current().params._id).url));
+        Player.play();
       }
     });
-	vid=$("#videoDisplayId").get(0);
-	vidCtrl=new videoControler(vid,30,[100,200,300,400]);
 });
 
 Template.videoPlayer.onDestroyed(function(){
