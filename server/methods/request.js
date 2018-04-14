45f97f32e3c825418767246c7d2b9d927ec11205
redeyes361 <redeyes_361@hotmail.com>
2018-04-14 19:11:05 +0200

    chnge requests

--- server/methods/request.js
+++ server/methods/request.js
@@ -20,7 +20,7 @@ Meteor.methods({
     },
 
     /**
-     * Htttp request that ask the parameters of an extraction tool
+     * Http request that ask the parameters of an extraction tool
      *
      * @param extractorUrl The url of the extractor we want to get the parameters
      * @returns {String} The string representation of a xsd file which contain the definition of the parameters
@@ -35,13 +35,13 @@ Meteor.methods({
     /**
      *
      * Http request that send a request of annotations to an extraction service
-     * @param _id
-     * @param params
-     * @param extractorUrl
-     * @returns {boolean}
+     * @param _id the id of the project
+     * @param params the params for the extractor
+     * @param extractorUrl the url of the extractor
+     * @returns {boolean} response's status code is 200
      */
     putRequest: (_id, params, extractorUrl)=>{
-        var url = "http://"+extractorUrl+"/param/"+id;
+        var url = "http://"+extractorUrl+"/param/"+_id;
         const result = HTTP.call("PUT",url,{data:{param: params}});
         if(result.statusCode == 200){
             return true;
@@ -53,14 +53,15 @@ Meteor.methods({
 
     /**
      * Http request that create a folder in a extraction service
-     * @param _id
-     * @param extractorUrl
-     * @returns {boolean}
+     * @param _id the id of the project
+     * @param extractorUrl the url of the extractor
+     * @returns {boolean} the response's status ode is 200
      */
-    initRequest: (_id,extractorUrl)=>{
+    initRequest: (_id,extractorUrl,_checksum,_downUrl)=>{
 
-        var url = "http://"+extractorUrl+"/creation/"+id;
-        const result = HTTP.call("PUT",url);
+        var url = "http://"+extractorUrl+"/creation/"+_id;
+        console.log(url);
+        const result = HTTP.call("PUT",url,{data:{checksum: _checksum, url:_downUrl}});
         if(result.statusCode==200){
             return true;
         }else{
