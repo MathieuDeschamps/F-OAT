509888c34d90944a19b63c519e7c7fd1306a2025
Ludovic Landuré <33468282+LudovicLandure@users.noreply.github.com>
2018-05-11 02:12:10 +0200

    Add files via upload

--- client/views/components/configurator/configurator.js
+++ client/views/components/configurator/configurator.js
@@ -9,58 +9,32 @@ import {XMLGenerator} from '../XMLGenerator/XMLGenerator.js';
 Template.configurator.onRendered(()=>{
     console.log('Lecture test.xsd');
 	Meteor.call("getXml",'/parameters.xsd',(err,result)=>{
-	if(err){
-		alert(err.reason);
-	}else{
-		console.log('parameters.xsd : ', result.data);
-		var xsd=$.parseXML(result.data);
-		console.log('XSD parsé : ',xsd);
+		if(err){
+			alert(err.reason);
+		}else{
+			console.log('parameters.xsd : ', result.data);
+			var xsd=$.parseXML(result.data);
+			console.log('XSD parsé : ',xsd);
 		
-		var xsdObj=new XSDObject(xsd);
-		console.log('xsdObj',xsdObj);
-		
-		//XMLXSDGen=new XMLXSDGenerator();
-		Meteor.call("getXml",'/test.xml',(err,result)=>{
-			/*console.log('test.xml : ', result.data);
-			var xml=$.parseXML(result.data);
-			console.log('XML parsé : ',xml);*/
-			xmlxsdObj= new XMLXSDObj(undefined,xsdObj);
-			xmlxsdForm=new XMLXSDForm(xmlxsdObj,'testId','testName','configurator');
+			var xsdObj=new XSDObject(xsd);
+			console.log('xsdObj',xsdObj);
+
+			var xmlxsdObj= new XMLXSDObj(undefined,xsdObj);
+
+			var xmlxsdForm=new XMLXSDForm(xmlxsdObj,'testId','testName','configurator');
 			xmlxsdForm.generate();
-			//$("#configurator").html(xmlxsdForm.html);
+
 			$("#configuratorButton").append(
-				'<div> <a id="finalButton" class="waves-effect waves-light btn">Apply Configuration</a></div>'
+				'<div><a id="finalButton" class="waves-effect waves-light btn col s6 offset-s3">Launch Extraction</a></div>'
 			);
+
 			$('#finalButton').click(function(){
 				gen=new XMLGenerator(xmlxsdObj);
 				console.log(gen.generateXML());
 			});
-		});
-		
-		
-		
-		/*
-		$xsd=$(xsd);
-		var $schema = $xsd.find("xs\\:schema");
-		
-		
-		var i=0; 
-		console.log("schema children", $schema.children());
-		$schema.children().each(function(){
-			var name = $(this).attr('name');
-			i=i+1;
-			console.log('numéro ',i,' : ',name);
-			j=0;
-            $(this).children().each(function(i,child){
-				console.log("this", this);
-				var tag=$(this).prop('nodeName');
-				console.log('tag ',j,' : ',tag);
-				j++;
-				
-			})
-		});*/
-    }
+		}
 	});
+
 });
 
 /*
