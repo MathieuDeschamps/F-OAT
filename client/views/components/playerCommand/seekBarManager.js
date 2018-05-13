863f8998850aebd4c8ca338e5f15c0921026a668
Ludovic Landuré <33468282+LudovicLandure@users.noreply.github.com>
2018-05-13 11:12:27 +0200

    playerCommand and videocontroler update

---
+++ client/views/components/playerCommand/seekBarManager.js
@@ -0,0 +1,28 @@
+export class seekBarManager {
+	constructor(videoControler){
+		this.videoCtrl=videoControler;
+		this.follow=1;
+	}
+	
+	mousePressed(){
+		this.follow = 0;
+	}
+	
+	mouseReleased(){
+		this.follow = 1;
+		this.refresh();
+	}
+	
+	notify(currentFrame){
+		//console.log("seekBar");
+		if (this.follow == 1){
+			$( "#seekBar" ).val(currentFrame);
+		}
+		//this.refresh();
+		//console.log($( "#seekBar" ).val().toString());
+	}
+	
+	refresh(){
+		$( "#seekBar" ).val(this.videoCtrl.getCurrentFrame());
+	}
+}
